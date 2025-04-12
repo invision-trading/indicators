f0b619c7b457ebf300c19e9763b5b2f811547271
Jacob Peterson <14127217+Petersoj@users.noreply.github.com>
2025-04-12 14:37:09 -0700

    Add Lombok dependency, JReleaser publishing config, initial README

---
+++ README.md
@@ -0,0 +1,5 @@
+# Indicators
+
+A Java library that provides a variety of statistical, utility, and technical
+[indicators](https://www.investopedia.com/terms/i/indicator.asp) for time series data, such as
+[OHLC bars](https://www.investopedia.com/terms/c/candlestick.asp).
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import org.jreleaser.model.Active.ALWAYS
+import org.jreleaser.model.Active.NEVER
+
 plugins {
     `java-library`
     `maven-publish`
@@ -18,6 +21,11 @@ dependencies {
     // Google Guava
     implementation("com.google.guava", "guava", "33.2.1-jre")
 
+    // Project Lombok
+    val lombokVersion = "1.18.36"
+    implementation("org.projectlombok", "lombok", lombokVersion)
+    annotationProcessor("org.projectlombok", "lombok", lombokVersion)
+
     // num
     api("trade.invision", "num", "1.2.0")
 }
@@ -33,5 +41,69 @@ tasks.javadoc.configure {
     options {
         (this as CoreJavadocOptions).addBooleanOption("Xdoclint:none", true)
         addStringOption("link", "https://docs.oracle.com/en/java/javase/21/docs/api/")
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifactId = "indicators"
+            from(components["java"])
+            pom {
+                name = "indicators"
+                description = "A Java library that provides a variety of statistical, utility, and technical " +
+                        "indicators for time series data, such as OHLC bars."
+                url = "https://github.com/invision-trading/indicators"
+                inceptionYear = "2025"
+                licenses {
+                    license {
+                        name = "MIT License"
+                        url = "https://opensource.org/licenses/MIT"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "Petersoj"
+                        name = "Jacob Peterson"
+                    }
+                }
+                scm {
+                    connection = "scm:git:https://github.com/invision-trading/indicators.git"
+                    developerConnection = "scm:git:ssh://github.com/invision-trading/indicators.git"
+                    url = "https://github.com/invision-trading/indicators"
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            url = uri(layout.buildDirectory.dir("staging-deploy"))
+        }
+    }
+}
+
+jreleaser {
+    signing {
+        active = ALWAYS
+        armored = true
+    }
+    deploy {
+        maven {
+            mavenCentral {
+                create("sonatype") {
+                    active = ALWAYS
+                    url = "https://central.sonatype.com/api/v1/publisher"
+                    stagingRepository("build/staging-deploy")
+                    // Timeout of 1 hour.
+                    maxRetries = 60
+                    retryDelay = 60
+                }
+            }
+        }
+    }
+    release {
+        github {
+            uploadAssets = NEVER
+        }
     }
 }
